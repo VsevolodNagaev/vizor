7ac433c0f99dc9b00bc9e66690d3cadeed9436fe
Vsevolod <48849178+VsevolodNagaev@users.noreply.github.com>
2023-02-25 00:32:16 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    $data = $_GET['data'];
-    $time = time();
+// Set the response header to JSON
+header('Content-Type: application/json');
 
-    // Save the data and time to a file or database
+// Get the current time
+$time = time();
 
-    $response = array('data' => $data, 'time' => $time);
-    header('Content-Type: application/json');
-    echo json_encode($response);
-}
+// Format the time as a string
+$time_str = date('Y-m-d H:i:s', $time);
+
+// Create a JSON object with the time
+$json_obj = array(
+    'time' => $time_str
+);
+
+// Convert the JSON object to a string
+$json_str = json_encode($json_obj);
+
+// Return the JSON string as the response
+echo $json_str;
 ?>
