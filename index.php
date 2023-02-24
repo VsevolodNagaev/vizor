5a30961306e474cff9f8b8945bb2342ce44743a1
Vsevolod <48849178+VsevolodNagaev@users.noreply.github.com>
2023-02-25 00:39:30 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
-// Set the response header to JSON
-header('Content-Type: application/json');
 
-// Get the current time
-$time = time();
+echo "Data: " . $data_value . "\n";
+echo "Time: " . $time_value;
 
-// Format the time as a string
-$time_str = date('Y-m-d H:i:s', $time);
-
-// Create a JSON object with the time
-$json_obj = array(
-    'time' => $time_str
-);
-
-// Convert the JSON object to a string
-$json_str = json_encode($json_obj);
-
-// Return the JSON string as the response
-echo $json_str;
 ?>
