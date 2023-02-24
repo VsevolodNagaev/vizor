da82ff1ceec7d4dedaccf2e1fd9a9021c5a38783
Vsevolod <48849178+VsevolodNagaev@users.noreply.github.com>
2023-02-25 00:41:54 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 
-echo "Data: " . $data_value . "\n";
-echo "Time: " . $time_value;
+// Create an array with data and time fields
+$response = array(
+    'data' => 'some data',
+    'time' => date('Y-m-d H:i:s')
+);
+
+// Encode the array into JSON format
+$json = json_encode($response);
+
+// Set the HTTP Content-Type header to application/json
+header('Content-Type: application/json');
+
+// Send the JSON response to the client
+echo $json;
 
 ?>
