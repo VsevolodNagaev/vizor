c5d824209323acd851f51a47b4252c825bdf9fbc
Vsevolod <48849178+VsevolodNagaev@users.noreply.github.com>
2023-02-25 09:04:58 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,7 @@
 <?php
+$data = "Hello World"; // Replace with your actual data
+$time = date("Y-m-d H:i:s"); // Get the current time
 
-// Create an array with data and time fields
-$response = array(
-    'data' => 'some data',
-    'time' => date('Y-m-d H:i:s')
-);
-
-// Encode the array into JSON format
-$json = json_encode($response);
-
-// Set the HTTP Content-Type header to application/json
-header('Content-Type: application/json');
-
-// Send the JSON response to the client
-echo $json;
-
+// Return the data and the current time in JSON format
+echo json_encode(array("data" => $data, "time" => $time));
 ?>
